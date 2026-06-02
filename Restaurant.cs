385b75855e201d12ce8fc77aca5a8f8ac8f9dc85
vladyslavstepanenko25 <vladyslav.stepanenko@nure.ua>
2026-06-02 17:27:02 +0300

    review-stepanenko
    Corrected XML documentation for return value in GetTopDishes method.

--- Restaurant.cs
+++ Restaurant.cs
@@ -66,7 +66,7 @@ namespace RestaurantApp
         /// Повертає список перших N страв.
         /// </summary>
         /// <param name="count">Кількість страв для отримання</param>
-        /// <returns>Список назв страв</returns>
+        /// <returns>Список назв страв</returns>   
         public List<string> GetTopDishes(int count)
         {
             // Якщо передано від'ємне число, прирівнюємо до 0
